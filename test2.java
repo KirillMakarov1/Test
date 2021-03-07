96ebf2cf703f1e7377719afae746a3a38aa7dfeb
Kirill <u.kirillmakarov@gmail.com>
2021-03-07 13:57:19 +0300

    Вернули все обратно

--- test2.java
+++ test2.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число:");12
+        System.out.println("Введите число:");
 
         if (!sc.hasNextInt()) {
             System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
