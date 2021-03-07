17dc5e2cb4cf42b06cdd2e6cbf3a951c5d797b10
Kirill <u.kirillmakarov@gmail.com>
2021-03-07 13:28:09 +0300

    Изменили что-то

--- test2.java
+++ test2.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число:");
+        System.out.println("Введите число:");12
 
         if (!sc.hasNextInt()) {
             System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
